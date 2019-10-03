e31637f54f25fde34d0ab33baa597b8b5afbfa25
TJ Hooker <tyrelhooker@gmail.com>
2019-10-02 22:26:09 -0400

    Dynamically create html with scripts file and update Readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # simple_API_App
+Created using Tania Rascia's tutorial. Purpose is to review vanilla js HTTP requests (XHR) as an alternative to using the axios library. This app will be refactored using the native Fetch API in another project. 
--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
+const app = document.getElementById('root');
+
+const logo = document.createElement('img');
+logo.src = 'logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
 let request = new XMLHttpRequest();
 
 console.log({request});
@@ -10,10 +21,30 @@ request.onload = function() {
   let data = JSON.parse(this.response);
   console.log({data});
 
-  data.forEach(movie => {
-    console.log(movie.title);
-  })
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+
+      const h1 = document.createElement('h1');
+      h1.textContent = movie.title;
+
+      const p = document.createElement('p');
+      movie.description = movie.description.substring(0, 300);
+      p.textContent = `${movie.description}...`
+
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
+    });
+  } else {
+    const errorMessage = document.createElement('h2');
+    errorMessage.textContent = 'For some reason it is not working';
+    app.appendChild(errorMessage);
+  }
 }
 
 request.send();
+
+
 
